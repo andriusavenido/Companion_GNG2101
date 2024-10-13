804ed64d9958799178186b89a013e3d244e4ad9b
Andrius <andriusavenido@gmail.com>
2024-10-12 21:58:35 -0400

    adding models

---
+++ server/src/models/conversationModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+/**
+ * Schema for storing individual messages in a conversation.
+ * @property {string} sender - The sender of the message ('user' or 'bot').
+ * @property {string} text - The content of the message.
+ */
+const messageSchema = new Schema({
+    sender:{
+        type: String,
+        enum: ['user', 'bot'],
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    }
+});
+
+/**
+ * Schema for storing a log of a user's conversation with ChatGPT.
+ * @property {Schema.Types.ObjectId} userId - The ID of the user having the conversation.
+ * @property {Message[]} messages - An array of message objects representing the conversation.
+ * @property {Date} timestamp - The timestamp of the conversation
+ */
+const conversationSchema = new Schema ({
+    userId:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required:'true',
+    },
+    messages:[messageSchema],
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+module.exports = mongoose.model('Conversation',conversationSchema);
